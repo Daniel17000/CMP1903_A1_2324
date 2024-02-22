087f67c122d04ed9c4c033d0de76dbfa252bbe5c
Daniel Hey <danieljameshey@outlook.com>
2024-02-22 13:24:38 +0000

    Added more comments
    Added more comments so its easier to understand

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -8,17 +8,18 @@ namespace CMP1903_A1_2324
 {
     public class Die
     {
+        //Private fields to store values
         private int currentValue;
         private Random random;
 
         public Die()
         {
-            random = new Random();
+            random = new Random();      //initialises the Random object to generate random numbers
         }
 
         public int Roll()
         {
-            currentValue = random.Next(1,7);
+            currentValue = random.Next(1,7); // This is inclusive, gets a number between 1 and 6
             return currentValue;
         }
     }
--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -8,11 +8,13 @@ namespace CMP1903_A1_2324
 {
     public class Game
     {
-        public Die[] dice;
+        public Die[] dice;      // Creates an array to hold dice objects
         
         public Game() 
         { 
-            dice = new Die[3];
+            dice = new Die[3];          // initalises and creates three dice objects
+
+            // Creates three die objects and will assign them to the dice array
             for (int i = 0; i < 3; i++)
             {
                 dice[i] = new Die();
@@ -23,17 +25,21 @@ namespace CMP1903_A1_2324
         {
             int total = 0;
             int[] rolls = new int[3];
+
+            // Rolls each dice to get a value and stores it in the array 
             for (int i = 0; i < 3; i++)
             {
                 rolls[i] = dice[i].Roll();
                 total += rolls[i];
             }
 
+            //Displays all of the dice rolls and the total sum
             Console.WriteLine("Individual die rolls: " + string.Join(", ", rolls));
             Console.WriteLine("Total of the three dice rolls; " + total);
             return total;
         }
 
+        // Allows access for the dice array from outside of the game class
         public Die[] Dice
         {
             get 
