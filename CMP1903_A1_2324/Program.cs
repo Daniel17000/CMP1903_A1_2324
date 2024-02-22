d4610497ccbc189cde83b4ae1544b7e7ee884030
Daniel Hey <danieljameshey@outlook.com>
2024-02-22 12:50:24 +0000

    Updated Program.CS
    Added the code for the Program

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -6,14 +6,22 @@ using System.Threading.Tasks;
 
 namespace CMP1903_A1_2324
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            /*
-             * Create a Game object and call its methods.
-             * Create a Testing object to verify the output and operation of the other classes.
-             */
+            Program.Run();
+        }
+
+        public static void Run()
+        {
+            // Creating a game object
+            Game game = new Game();
+            // Rolling the dice for the turn
+            int total = game.RollAllTheDice();
+            // Will perform all the testing in place
+            Testing.Test();
+
         }
     }
 }
