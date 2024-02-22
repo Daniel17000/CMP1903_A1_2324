e2ad554bda5bb35ad175075af5f9784e79c65f3f
Daniel Hey <danieljameshey@outlook.com>
2024-02-22 13:31:45 +0000

    Updated Die.cs
    The random numbers were the same so a single instance of random had to be created

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -8,14 +8,12 @@ namespace CMP1903_A1_2324
 {
     public class Die
     {
+
+        private static Random random = new Random();    // This is to hold a single instance of Random
+
         //Private fields to store values
         private int currentValue;
-        private Random random;
-
-        public Die()
-        {
-            random = new Random();      //initialises the Random object to generate random numbers
-        }
+        
 
         public int Roll()
         {
